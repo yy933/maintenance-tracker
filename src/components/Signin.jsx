e6c3e16772f5d70d8796893a14c2016a86010270
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-03 18:50:02 +0800

    feat: update href to root on <a> element of the Header

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -39,7 +39,7 @@ export default function Signin() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="/" className="flex items-center gap-2 font-medium">
             <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,7 +6,7 @@ export default function Signup() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <a href="/" className="flex items-center gap-2 font-medium">
             <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
