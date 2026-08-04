29538827d7e090d23a58f6fbf2f0222d23e0df7f
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-04 13:54:20 +0800

    feat: update login form signinWithGoogle logic and UI

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -35,6 +35,8 @@ export default function Signin() {
     }
   }, [state, session, navigate]);
 
+  
+
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
